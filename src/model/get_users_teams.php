66426a6c34cb134b2b527db6f2da9b199168a535
Julio Herrera <julio@debian>
2019-06-27 13:31:30 -0400

    Returns empty users when  there is none Adding some comments

--- src/model/get_users_teams.php
+++ src/model/get_users_teams.php
@@ -1,14 +1,15 @@
 <?php
+$users = [];
 
 $sql = 'SELECT * FROM users';
 $users_result = mysqli_query($db, $sql);
 if (mysqli_num_rows($users_result) > 0) {
+  // Statement for getting the teams of a user
   $sql = 'SELECT t.name 
   FROM teams_users as tu INNER JOIN teams as t ON tu.team_id = t.id
   WHERE user_id=?';
   $query_team_stmt = mysqli_prepare($db, $sql);
 
-  $users = [];
   while ($user = mysqli_fetch_assoc($users_result)) {
     mysqli_stmt_bind_param($query_team_stmt, "s", $user['id']);
 
