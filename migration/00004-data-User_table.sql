b585f0448c1bcf50dad2579623a9766363e04f83
Julio Herrera <julio@debian>
2019-06-27 11:09:23 -0400

    Adding fake data for Users table

---
+++ migration/00004-data-User_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `created_at`) VALUES 
+  (NULL, 'Scott', 'Sommerfeldt', current_timestamp()), 
+  (NULL, 'Julio', 'Herrera', current_timestamp()), 
+  (NULL, 'Jhon', 'Smith', current_timestamp())
